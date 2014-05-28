c33689671f73f39f5c93661b72fb9b2fddd2712e
movsesiv <movsesiv@gmail.com>
2014-05-28 23:31:06 +0400

    add packages

---
+++ src/packages/Incoding.MSpecContrib.1.3.0.789-beta/content/MSpecAssemblyContext.cs.pp
@@ -0,0 +1,34 @@
+﻿namespace $rootnamespace$
+{
+
+    using Incoding.MSpecContrib;
+    using Machine.Specifications;
+    using System.Configuration;
+	using FluentNHibernate.Cfg;
+    using FluentNHibernate.Cfg.Db;
+
+	////ncrunch: no coverage start	
+    public class MSpecAssemblyContext : IAssemblyContext
+    {
+        #region IAssemblyContext Members
+
+        public void OnAssemblyStart()
+        {
+		            var configure = Fluently
+                    .Configure()
+                    .Database(MsSqlConfiguration.MsSql2008
+                                                .ConnectionString(ConfigurationManager.ConnectionStrings["Test"].ConnectionString)
+                                                .ShowSql())
+                    .Mappings(configuration => configuration.FluentMappings.AddFromAssembly(typeof(Bootstrapper).Assembly));
+
+           PleasureForData.StartNhibernate(configure, true);
+        }
+
+        public void OnAssemblyComplete()
+        {		  
+        }
+
+        #endregion
+    }
+    ////ncrunch: no coverage end
+}
