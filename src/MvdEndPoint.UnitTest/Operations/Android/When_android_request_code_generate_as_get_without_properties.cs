d4faed07296379af89c739f20debca2b1778a78e
vkopachinsy <vlad.kopachinsky@gmail.com>
2015-03-17 19:13:06 +0300

    update framework

--- src/MvdEndPoint.UnitTest/Operations/Android/When_android_request_code_generate_as_get_without_properties.cs
+++ src/MvdEndPoint.UnitTest/Operations/Android/When_android_request_code_generate_as_get_without_properties.cs
@@ -15,6 +15,28 @@
     [Subject(typeof(AndroidRequestCodeGenerateQuery))]
     public class When_android_request_code_generate_as_get_without_properties
     {
+        Establish establish = () =>
+                              {
+                                  var query = Pleasure.Generator.Invent<AndroidRequestCodeGenerateQuery>(dsl => dsl.Tuning(r => r.Type, typeof(GetCustomerQuery)));
+                                  expected = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, typeof(When_android_request_code_generate_as_get_without_properties).Name));
+
+                                  mockQuery = MockQuery<AndroidRequestCodeGenerateQuery, string>
+                                          .When(query)
+                                          .StubQuery(Pleasure.Generator.Invent<GetMetaFromTypeQuery>(dsl => dsl.Tuning(r => r.Type, typeof(GetCustomerQuery))),
+                                                     Pleasure.Generator.Invent<GetMetaFromTypeQuery.Response>(dsl => dsl.Tuning(r => r.Name, "GetCustomerQuery")
+                                                                                                                        .Tuning(r => r.Package, "com.qabenchmarking.android.models.GetCustomerQuery")
+                                                                                                                        .Tuning(r => r.Namespace, "com.qabenchmarking.android.models")))
+                                          .StubQuery(Pleasure.Generator.Invent<GetNameFromTypeQuery>(dsl => dsl.Tuning(r => r.Mode, GetNameFromTypeQuery.ModeOf.Request)
+                                                                                                               .Tuning(r => r.Type, query.Type)), "GetCustomerRequest")
+                                          .StubQuery(Pleasure.Generator.Invent<GetPropertiesByTypeQuery>(dsl => dsl.Tuning(r => r.Device, DeviceOfType.Android)
+                                                                                                                   .Tuning(r => r.IsCommand, false)
+                                                                                                                   .Tuning(r => r.Type, typeof(GetCustomerQuery))), new List<GetPropertiesByTypeQuery.Response>());
+                              };
+
+        Because of = () => mockQuery.Original.Execute();
+
+        It should_be_result = () => mockQuery.ShouldBeIsResult(s => s.ShouldEqual(expected));
+
         #region Fake classes
 
         class GetCustomerQuery : QueryBase<string>
@@ -34,26 +56,5 @@
         static string expected;
 
         #endregion
-
-        Establish establish = () =>
-                                  {
-                                      var query = Pleasure.Generator.Invent<AndroidRequestCodeGenerateQuery>(dsl => dsl.Tuning(r => r.Type, typeof(GetCustomerQuery)));
-                                      expected = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, typeof(When_android_request_code_generate_as_get_without_properties).Name));
-
-                                      mockQuery = MockQuery<AndroidRequestCodeGenerateQuery, string>
-                                              .When(query)
-                                              .StubQuery(Pleasure.Generator.Invent<GetMetaFromTypeQuery>(dsl => dsl.Tuning(r => r.Type, typeof(GetCustomerQuery))),
-                                                         Pleasure.Generator.Invent<GetMetaFromTypeQuery.Response>(dsl => dsl.Tuning(r => r.Name, "GetCustomerQuery")
-                                                                                                                            .Tuning(r => r.Package, "com.qabenchmarking.android.models.GetCustomerQuery")
-                                                                                                                            .Tuning(r => r.Namespace, "com.qabenchmarking.android.models")))
-                                              .StubQuery(Pleasure.Generator.Invent<GetNameFromTypeQuery>(dsl => dsl.Tuning(r => r.Mode, GetNameFromTypeQuery.ModeOf.Request)
-                                                                                                                   .Tuning(r => r.Type, query.Type)), "GetCustomerRequest")
-                                              .StubQuery(Pleasure.Generator.Invent<GetPropertiesByTypeQuery>(dsl => dsl.Tuning(r => r.Device, DeviceOfType.Android)
-                                                                                                                       .Tuning(r => r.Type, typeof(GetCustomerQuery))), new List<GetPropertiesByTypeQuery.Response>());
-                                  };
-
-        Because of = () => mockQuery.Original.Execute();
-
-        It should_be_result = () => mockQuery.ShouldBeIsResult(s => s.ShouldEqual(expected));
     }
 }
